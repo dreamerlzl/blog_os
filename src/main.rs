53cbc6b110103c46c189fbe83b37a4d0677de7e2
dreamerlzl <875350879@qq.com>
2023-01-08 17:41:06 +0800

    feat: add vga buffer

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 #![no_std]
 #![no_main]
 
-fn main() {}
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-// extern "C" -> telling compilter should use the C calling convention
-// the name _start here matters, so no name mangling
+// extern "C" -> telling compiler should use the C calling convention
+// the name _start here matters(marks the function as lang_start), so no name mangling
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    panic!("fuck");
     loop {}
 }
